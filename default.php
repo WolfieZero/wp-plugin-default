acf47c87d7de91b1bd517774f90be636d822af75
Neil Sweeney <neil@wolfiezero.com>
2014-02-04 18:06:15 +0000

    initial commit

---
+++ default.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * ============================================================================
+ * ...
+ * ============================================================================
+ *
+ * @package     WordPress
+ * @subpackage  Plugin: ...
+ * @author      Neil Sweeney <neil.sweeney@fubra.com>
+ * @license     GPL-2.0+
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * @wordpress-plugin
+ * Plugin Name:  ...
+ * Description:  ...
+ * Version:      1.0.0
+ * Author:       Neil Sweeney <neil@wolfiezero.com>
+ * Author URI:   http://wolfiezero.com/
+ * License:      GPL-2.0+
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI:  wolfiezero/...
+ * GitHub Branch:      master
+ */
+
+
+// ----------------------------------------------------------------------------
+// Accessed Directly
+// ----------------------------------------------------------------------------
+
+if( !defined( 'WPINC' ) ) die;
+
+
+// ----------------------------------------------------------------------------
+// Requires
+// ----------------------------------------------------------------------------
+
+// require plugin_dir_path( __FILE__ ) . 'class-....php';
+
+
+// ----------------------------------------------------------------------------
+// Hooks
+// ----------------------------------------------------------------------------
+
+// register_activation_hook( __FILE__, array( 'TwitterAPI', 'activate' ) );
+// register_deactivation_hook( __FILE__, array( 'TwitterAPI', 'deactivate' ) );
+
+// Actions
+// add_action( '...', array( '...', '...'' ) );
+
+// Filters
+// add_filter( '...', array( '...', '...'' ) );
+
+
+// ----------------------------------------------------------------------------
+// Admin Stuff
+// ----------------------------------------------------------------------------
+
+if( is_admin() && ( !defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
+
+}
